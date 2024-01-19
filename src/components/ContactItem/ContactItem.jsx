82273e923cbe63d698c3a91e5e75059ccf2fa251
deathfluffy <mrandreu454@gmail.com>
2024-01-19 18:41:30 +0200

    +++

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,12 +1,15 @@
 import propTypes from 'prop-types';
 import css from './ContactItem.module.css'
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 
-export const ContactItem = ({ id, name, number, onDelete }) =>  {
+export const ContactItem = ({ id, name, number }) =>  {
+  const dispatch = useDispatch();
   return (
     <div className={css.item}>
       <span>{name}:</span>
       <span>{number}</span>
-      <button className={css.delButton} type="button" onClick={() => onDelete(id)}>
+      <button className={css.delButton} type="button" onClick={() => dispatch(deleteContact({id: id}))}>
         Delete
       </button>
     </div>
